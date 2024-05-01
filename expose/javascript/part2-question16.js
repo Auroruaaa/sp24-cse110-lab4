f9cf71b1792320154c0d9a68e3256025fc9aecde
Beijie Cheng <116754028+Auroruaaa@users.noreply.github.com>
2024-04-30 21:59:44 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,3 +1,4 @@
+// The given variable statistics
 let statistics = {
   redCars: 21,
   blueCars: 45,
@@ -7,10 +8,13 @@ let statistics = {
   rareCars: 2
 };
 
+// a for loop to print out required property
 for (let property in statistics) {
+  // check if the property starts with letter 'r'
   if (property[0] === 'r') {
       console.log(`${property}: ${statistics[property]}`);
   }
+  // if not, check if the value of the property is an odd number
   else if ((statistics[property] % 2) == 1) {
     console.log(`${property}: ${statistics[property]}`);
   }
