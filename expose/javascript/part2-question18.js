db9077a1c6dc2c376f2aa5ab09002af87b05da8b
Beijie Cheng <116754028+Auroruaaa@users.noreply.github.com>
2024-04-30 22:36:15 -0700

    Update part2-question18.js

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,1 +1,10 @@
+// print out the current time every second
+function showtime() {
+    let d = new Date();
+    let time = d.toLocaleTimeString();
+    console.log(time);
+}
 
+let IntervId = setInterval(showtime, 1000);
+
+// IclearInterval(IntervId);
